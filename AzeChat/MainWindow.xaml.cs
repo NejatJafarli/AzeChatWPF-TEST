f1bdafcba0295933db69b72e6e774eede924fd30
nicat ceferli <mega.ceferli@gmail.com>
2021-09-12 18:58:48 +0400

    Add project files.

---
+++ AzeChat/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AzeChat
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var temp = DateTime.Now;
+			TimeText.Text = temp.Hour.ToString();
+			TimeText.Text += ":";
+			TimeText.Text += temp.Minute.ToString();
+
+			DispatcherTimer timer= new DispatcherTimer();
+			timer.Interval = TimeSpan.FromSeconds(30);
+			timer.Tick += Timer_Tick;
+			timer.Start();
+		}
+
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			var temp=DateTime.Now;
+			TimeText.Text = temp.Hour.ToString();
+			TimeText.Text += ":";
+			TimeText.Text += temp.Minute.ToString();
+		}
+		void TopGrid_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			//you must be change there Event useButtonState.Presse )
+			if (e.LeftButton == MouseButtonState.Pressed)
+			{
+				DragMove();
+			}
+		}
+
+		private void ImageBrush_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (e.LeftButton==MouseButtonState.Released)
+			{
+
+			}
+		}
+	}
+}
---
+++ AzeChat/MyUserMessageUC.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace AzeChat
+{
+	/// <summary>
+	/// Interaction logic for MyUserMessageUC.xaml
+	/// </summary>
+	public partial class MyUserMessageUC : UserControl
+	{
+		public MyUserMessageUC()
+		{
+			InitializeComponent();
+		
+			TimeText.Text =DateTime.Now.Hour.ToString();
+			TimeText.Text += ":";
+			TimeText.Text +=DateTime.Now.Minute.ToString();
+		}
+
+
+}
+}
